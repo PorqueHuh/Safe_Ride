55c89eab1b6007c19802ff7e76951e7d7027b4b3
PorqueHuh <33770925+PorqueHuh@users.noreply.github.com>
2019-04-07 14:51:45 -0500

    test

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Button } from '../components/Button';
-
+import Dialog from "react-native-dialog";
 
 
 export default class HomeScreen extends Component {
+state = {
+    dialogVisible1: false,
+    dialogVisible2: false
+};
+
     static navigationOptions = {
-        title: 'Home',
         headerStyle: {
             backgroundColor: 'red',
           },
@@ -18,11 +22,54 @@ export default class HomeScreen extends Component {
         },
     };
 
+    showDialog = () => {
+        this.setState({ dialogVisible1: true });
+    }
+
+    handleYes = () => {
+        this.setState({ dialogVisible1: false });
+    }
+
+    handleNo = () => {
+        this.setState({ dialogVisible1: false});
+    };
+    handleUpdate = () => {
+        this.setState({ dialogVisible1: false });
+    }
+    addCommentDialog = () => {
+        this.setState({ dialogVisible2: true });
+        <View>
+            <TouchableOpacity onPress={this.showDialog}>
+                    <Text>Add Comments</Text>
+                </TouchableOpacity>
+                <Dialog.Container visible={this.state.dialogVisible2}>
+                    <Dialog.Title>Confirm Destination</Dialog.Title>
+                    <Dialog.Description>Add comments for the drivers</Dialog.Description>
+                    <Dialog.Input label="Comments"></Dialog.Input>
+                    <Dialog.Button label="Confirm" onPress={this.handleNoLocation} />
+                </Dialog.Container>
+        </View>
+    }
+
     render() {
         return (
         <View style={styles.container}>
-            <Text>Welcome user!</Text>
-            <Text>What do you want to do?</Text>
+            <View>
+                <Text>Welcome user!</Text>
+                <Text>What do you want to do?</Text>
+            </View>
+            <View>
+                <TouchableOpacity onPress={this.showDialog}>
+                    <Text>Confirm</Text>
+                </TouchableOpacity>
+                <Dialog.Container visible={this.state.dialogVisible}>
+                    <Dialog.Title>Confirm Destination</Dialog.Title>
+                    <Dialog.Description>Destination is correct?.</Dialog.Description>
+                    <Dialog.Button label="Yes" onPress={this.addCommentDialog} />
+                    <Dialog.Button label="No" onPress={this.handleNo} />
+                    <Dialog.Button label="Update" onPress={this.handleUpdate} />
+                </Dialog.Container>
+        </View>
         </View>
         );
     }
