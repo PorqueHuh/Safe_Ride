d2727fd01373f014a0868dc0e6569ff02ddf0e49
PorqueHuh <33770925+PorqueHuh@users.noreply.github.com>
2019-04-16 16:04:32 -0500

    firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import RootNavigator from './navigators/RootNavigator';
-
+import firebase from 'firebase';
+import 'firebase/firestore';
 
 export default class App extends Component {
+
+  // Initialize Firebase
+componentWillMount() {
+  const settings = { timestampsInSnapshots: true };
+
+  const config = {
+  apiKey: "AIzaSyBRo4js_I1GWQhYM7tH_Ru-tAG5Tyx7-gQ",
+  authDomain: "saferide-c024a.firebaseapp.com",
+  databaseURL: "https://saferide-c024a.firebaseio.com",
+  projectId: "saferide-c024a",
+  storageBucket: "saferide-c024a.appspot.com",
+  messagingSenderId: "166980960789"
+  };
+
+  firebase.initializeApp(config);
+
+  firebase.firestore().settings(settings);
+}
   render() {
     return (
       <RootNavigator />
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Image, TextInput, Dimensions } from 'react-native';
-
+import firebase from 'firebase';
+import 'firebase/firestore';
 import { Button } from '../components/Button';
+import { Spinner } from '../components/Spinner';
 
 export default class Login extends Component {
   static navigationOptions = {
@@ -10,11 +12,53 @@ export default class Login extends Component {
         },
   };
 
+  constructor(props) {
+      super(props);
+      this.studentRef = firebase.firestore().collection('students');
+      this.unsubscribe = null;
+      this.state = {
+          students: [],
+          loading: true
+      }
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.studentRef.onSnapshot(this.onCollectionUpdate)
+  }
+
+  componentWillUnmount() {
+      this.unsubscribe();
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+      const students = [];
+
+      querySnapshot.forEach((doc) => {
+          const { id } = doc.data();
+          console.log(id);
+          students.push({
+              key: doc.id,
+              doc,
+              id
+          });
+      });
+
+      this.setState({
+          students,
+          loading: false,
+      });
+  }
+
   state = {
     ID: '',
   };
 
     render() {
+        if(this.state.loading) {
+            return (
+                <Spinner></Spinner>
+            )
+        }
         return (
             <View style={styles.container}>
                 <Image source={require('./img/Logo.png')} style={{flex: 1, alignSelf: 'center', height: 200, width: 200}} resizeMode="contain" />
