81ac47cb8d838841e0d09445cc50bfe96ee46a7d
PorqueHuh <33770925+PorqueHuh@users.noreply.github.com>
2019-04-23 23:41:41 -0500

    sending request

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,19 +3,27 @@ import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native
 import { Button } from '../components/Button';
 import MapView from 'react-native-maps'; //"npm install --save react-native-maps"
 import Dialog from 'react-native-dialog'; //"npm install --save react-native-dialog"
+import firebase from 'firebase'; //npm install firebase@5.0.3 --save
+import 'firebase/firestore';
 
 
 
 export default class HomeScreen extends Component {
     constructor(props){
         super(props);
-    
         this.state = {
             latitude : null,
             longitude : null,
             error: null,
-            //The state for button
             dialogVisible: false,
+            ADA: false,
+            comment: '',
+            guest: '',
+            id: '123456789',
+            location: 'Grove Street',
+            name: 'Carl Johnson',
+            phone: '4790001122',
+            confirmButton: true
         };
     }
     static navigationOptions = {
@@ -31,10 +39,42 @@ export default class HomeScreen extends Component {
         },
     };
 
+
     componentDidMount() {
+
+        const firestore = firebase.firestore()
+        const settings = {timestampsInSnapshots: true};
+        firestore.settings(settings);
+
+        const request = [];
+
+        firestore.collection('test').where('id', '==', this.state.id).get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                const { name, id, guest, comment, location } = doc.data();
+
+                request.push({
+                    key: doc.id,
+                    doc,
+                    name,
+                    id,
+                    guest,
+                    comment,
+                    location
+                });
+            });
+            console.log("Request: " +doc.date());
+        }).catch((error) => {
+            console.log('Doc doesnt exist, creating one');
+            firestore.collection('test').add({
+                id: this.state.id,
+            }).then(function() {
+                console.log('New doc sucessfully created');
+            });
+        });
+
+
     navigator.geolocation.getCurrentPosition(
        (position) => {
-         console.log("test");
          console.log(position);
          this.setState({
            latitude: position.coords.latitude,
@@ -56,6 +96,7 @@ export default class HomeScreen extends Component {
     };
 
     handleYes = () => {
+        this.sendRequest();
         this.setState({ dialogVisible: false});
     };
 
@@ -63,9 +104,64 @@ export default class HomeScreen extends Component {
         this.setState({dialogVisible: false});
     };
 
-    handleUpdate = () => {
-        this.setState({dialogVisible: false});
-    };
+    sendRequest = () => {
+        const firestore = firebase.firestore();
+        firestore.collection('test').where('id', '==', this.state.id).get().then((snapshot) => {
+            snapshot.forEach((doc) => {
+                if(doc.data().id === this.state.id) {
+                    firestore.doc('test/' + doc.id).set({
+                        ADA: this.state.ADA,
+                        comment: this.state.comment,
+                        guests: this.state.guest,
+                        id: this.state.id,
+                        location: this.state.location,
+                        name: this.state.name,
+                        phone: this.state.phone
+                    }).then(function() {
+                        console.log('Successful Request');
+                    });
+                }
+            });
+        });
+        this.setState({cancel: true})
+
+    }
+
+    renderConfirmButton = (props) => {
+        return (
+            <View>
+                <TouchableOpacity style = {styles.button} onPress={this.showDialog}>
+                <Text>Confirm</Text>
+                </TouchableOpacity>
+                <Dialog.Container visible={this.state.dialogVisible}>
+                <Dialog.Title>Confirm Destination</Dialog.Title>
+                <Dialog.Description>Destination is correct?</Dialog.Description>
+                <Text>{this.state.address}</Text>
+                <Dialog.Button label="Yes" onPress={this.handleYes} />
+                <Dialog.Button label="No" onPress={this.handleNo} />
+                <Dialog.Input label="Number of guest" onChangeText={(guest) => this.setState({guest: guest})} />
+                <Dialog.Input label="Additional comments" onChangeText={(comment) => this.setState({comment: comment})} />
+                </Dialog.Container>
+            </View>
+        )
+    }
+
+    renderCancelButton = (props) => {
+        return (
+            <View>
+                <TouchableOpacity style = {styles.button} onPress={this.showDialog}>
+                <Text>Cancel Request</Text>
+                </TouchableOpacity>
+                <Dialog.Container visible={this.state.dialogVisible}>
+                <Dialog.Description>Are you sure you want to cancel the request?</Dialog.Description>
+                <Dialog.Button label="Yes" onPress={this.cancelRequest} />
+                <Dialog.Button label="No" onPress={this.handleNo} />
+                </Dialog.Container>
+            </View>
+        )
+    }
+    
+
 
     render() {
     return (
@@ -84,19 +180,7 @@ export default class HomeScreen extends Component {
             />}
 
             </MapView>
-        
-            <View>
-                <TouchableOpacity style = {styles.button} onPress={this.showDialog}>
-                <Text>Confirm</Text>
-                </TouchableOpacity>
-                <Dialog.Container visible={this.state.dialogVisible}>
-                <Dialog.Title>Confirm Destination</Dialog.Title>
-                <Dialog.Description>Destination is correct?</Dialog.Description>
-                <Dialog.Button label="Yes" onPress={this.handleYes} />
-                <Dialog.Button label="No" onPress={this.handleNo} />
-                <Dialog.Button label="Update" onPress={this.handleUpdate} />
-                </Dialog.Container>
-            </View>
+                {this.renderConfirmButton()}
         </View>
     );
     }
@@ -124,9 +208,6 @@ const styles = {
     },
     buttonView: {
         flexDirection: 'row',
-    },
-    managerButtonView: {
-        justifyContent: 'flex-end',
     },
     container: {
         flex: 1,
